79b5445a376145eb5b39f733db8bb301a67ba18e
MichaelBrannon <musclem3mory@gmail.com>
2023-02-02 09:55:04 -1000

    py file, corr graph, and cleaned ipynb.

--- src/chess.py
+++ src/chess.py
@@ -45,7 +45,7 @@ def read_file(csv_file):
 
 # Grab and count the chess victory status, broken down by category. Make pie chart to depict the delineations.
 
-def chess_victories(chess_df):
+def chess_victories(chess_df, path="images/chess_outcomes_breakdown.png"):
      victory_status = chess_df['victory_status'].value_counts()
 
      fig, ax = plt.subplots(figsize = (10,6))
@@ -62,14 +62,14 @@ def chess_victories(chess_df):
      ax.set_title("Victory Status Breakdown")
      plt.tight_layout()
      plt.axis('equal')
-     plt.savefig("images/chess_outcomes_breakdown.png")
+     plt.savefig(path)
      return print({ 'Chess Outcomes Breakdown': victory_status})
 
 ###
 
 # Look at games won, by mate, out of time, and resignation, and compare to draws. Make pie charts for both delineations. 
 
-def wins_versus_draws(chess_games):
+def wins_versus_draws(chess_games, path="images/wins_versus_draws.png"):
     game_status = chess_games['victory_status'].value_counts('draw')
     game_status_percent = game_status * 100
     victories_percent = str(sum(game_status_percent.iloc[:3]))
@@ -91,7 +91,7 @@ def wins_versus_draws(chess_games):
     ax.set_title("Wins Versus Draws")
     plt.tight_layout()
     plt.axis('equal')
-    plt.savefig("images/wins_versus_draws.png")
+    plt.savefig(path)
     return print({ '% of Victories': victories_percent, '% of Draws': draws_percent})
 
 ###
@@ -105,7 +105,7 @@ def wins_versus_draws(chess_games):
 # Perform t-test to analyze the p-value and ultimately the null & alternate hypotheses for white differentials.
 # Histogram pot displaying white wins versus white draws when white is the superior opponent.
 
-def chess_differentials_white(chess_df):
+def chess_differentials_white(chess_df, path="images/white_wins_vs_draws_&_losses.png"):
     chess_df = chess_df.copy()
     chess_df['Rating Differential White'] = (chess_df['white_rating'] - chess_df['black_rating']).astype(int)
     black_wins = chess_df[(chess_df['Rating Differential White'] > 100) & (chess_df['winner'] == 'black')].value_counts()
@@ -122,7 +122,7 @@ def chess_differentials_white(chess_df):
     
     white_ttest = spicystats.ttest_ind(white_greater_100['Rating Differential White'], white_greater_100['winner'], equal_var = False)
     
-    fig,ax = plt.subplots()
+    fig, ax = plt.subplots()
 
     white_victory = white_greater_100[white_greater_100['winner'] == 1]
     white_draw_or_loss = white_greater_100[white_greater_100['winner'] == 0]
@@ -134,11 +134,12 @@ def chess_differentials_white(chess_df):
     ax.set_ylabel('# of Wins & Draws + Losses')
     ax.set_xlabel('Wins & Draws + Losses: Total Games [5,666]')
 
-    ax.legend();
-    plt.savefig("images/white_wins_vs_draws_&_losses.png")
+    ax.legend()
+    plt.savefig(path)
     white_win_percentage = (4110 /(4110+1556))*100
     white_draw_or_loss_percentage = (1556 /(4110+1556))*100
-    return print({ 'White Superior Games': white_greater_100, 'White T-test':white_ttest, 'White Victories': white_victory, 'White Draws or Losses': white_draw_or_loss, 'White Win %': white_win_percentage, 'White Draw or Loss %': white_draw_or_loss_percentage, 'Black Wins': black_wins})
+    print({ 'White Superior Games': white_greater_100, 'White T-test':white_ttest, 'White Victories': white_victory, 'White Draws or Losses': white_draw_or_loss, 'White Win Pct': white_win_percentage, 'White Draw or Loss Pct': white_draw_or_loss_percentage, 'Black Wins': black_wins})
+    return white_greater_100
 
 ###
 
@@ -146,7 +147,7 @@ def chess_differentials_white(chess_df):
 # Perform t-test to analyze the p-value and ultimately the null & alternate hypotheses for black differentials.
 # Histogram plot displaying black wins versus black draws and losses when black is the superior opponent.
 
-def chess_differentials_black(chess_df):
+def chess_differentials_black(chess_df, path="images/black_wins_vs_draws_&_losses.png"):
     chess_df = chess_df.copy()
     chess_df['Rating Differential Black'] = (chess_df['black_rating'] - chess_df['white_rating']).astype(int)
     white_wins = chess_df[(chess_df['Rating Differential Black'] > 100) & (chess_df['winner'] == 'white')].value_counts()
@@ -174,16 +175,18 @@ def chess_differentials_black(chess_df):
     ax.set_ylabel('# of Wins & Draws + Losses')
     ax.set_xlabel('Wins & Draws + Losses: Total Games [5,230]')
 
-    ax.legend();
-    plt.savefig("images/black_wins_vs_draws_&_losses.png")
+    ax.legend()
+    plt.savefig(path)
     black_win_percentage = (3623 /(3623+1607))*100
     black_draw_or_loss_percentage = (1607 /(3623+1607))*100
-
-    return print({' Black Superior Games': black_greater_100, 'Black T-test': black_ttest, 'Black Victories': black_victory, 'Black Draws or Losses': black_draw_or_loss, 'Black Win %': black_win_percentage, 'Black Draw or Loss %': black_draw_or_loss_percentage, 'White Wins': white_wins})
+    print({' Black Superior Games': black_greater_100, 'Black T-test': black_ttest, 'Black Victories': black_victory, 'Black Draws or Losses': black_draw_or_loss, 'Black Win Pct': black_win_percentage, 'Black Draw or Loss Pct': black_draw_or_loss_percentage, 'White Wins': white_wins})
+    return black_greater_100
 ###
 
 # Perform nonlinear Spearman correlation to see potential correlations between rating differentals, fow white and black respectively, and their victory status.
-def chess_correlations_spearman(white, black):
+def chess_correlations(white, black, path="images/correlations.png"):
+    white = white.copy()
+    black = black.copy()
     white_correlation_spearman = white['Rating Differential White'].corr(white['winner'], method ='spearman')
 
     black_correlation_spearman = black['Rating Differential Black'].corr(black['winner'], method ='spearman')
@@ -191,7 +194,25 @@ def chess_correlations_spearman(white, black):
     white_correlation_pearson = white['Rating Differential White'].corr(white['winner'], method ='pearson')
 
     black_correlation_pearson = black['Rating Differential Black'].corr(black['winner'], method ='pearson')
+
+    fig,ax = plt.subplots()
+    x_axis = [white_correlation_pearson, black_correlation_pearson, white_correlation_spearman, black_correlation_spearman]
+    x_axis_names = ['White Correlation Pearon', 'Black Correlation Pearson', 'White Correlation Spearman', 'Black Correlation Spearman']
+    c = ['silver', 'black', 'silver', 'black']
     
+    ax.set_ylim(0.15, 0.20)
+    plt.bar(x_axis_names, x_axis, color=c)
+    ax.set_title('Black and White Correlations')
+    ax.set_ylabel('Correlation Values')
+    ax.set_xlabel('Correlations')
+    plt.tight_layout()
+    fig.set_size_inches(16, 10)
+    plt.savefig(path)
+    white_pearson_legend = mpatches.Patch(color= 'silver', label='White Correlation Pearon: 0.1668')
+    black_pearson_legend = mpatches.Patch(color= 'black', label='Black Correlation Pearson: 0.1778')
+    white_spearman_legend = mpatches.Patch(color= 'silver', label='White Correlation Spearman: 0.1785')
+    black_spearman_legend = mpatches.Patch(color= 'black', label='Black Correlation Spearman: 0.1902')
+    plt.legend(handles=[white_pearson_legend, black_pearson_legend, white_spearman_legend, black_spearman_legend])
 
     return print({ 'Pearson White Correlation': white_correlation_pearson, 'Pearson Black Correlation': black_correlation_pearson, 'Spearman White Correlation': white_correlation_spearman, 'Spearman Black Correlation': black_correlation_spearman})
 
@@ -364,8 +385,6 @@ if __name__ == "__main__":
 
     chess_data = read_file("data/games.csv")
 
-    # chess_outcome = chess_outcome_breakdown(chess_data)
-
     # victories_breakdown = chess_victories(chess_data)
 
     # wins_and_draws = wins_versus_draws(chess_data)
@@ -374,7 +393,7 @@ if __name__ == "__main__":
 
     # chess_outcomes_black = chess_differentials_black(chess_data)
 
-    # chess_correlations = chess_correlations_spearman(chess_differentials_white(chess_data), chess_differentials_black(chess_data))
+    chess_corr = chess_correlations(chess_differentials_white(chess_data), chess_differentials_black(chess_data))
 
     # log_reg_white = test_train_white(chess_data)
 
